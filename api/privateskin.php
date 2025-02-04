62f233ae16da1dddf342d500594fd32fce62c570
wormps <wormpsps@gmail.com>
2025-02-04 23:52:32 +0300

    Add files via upload

---
+++ api/privateskin.php
@@ -0,0 +1,575 @@
+{
+  "success": true,
+  "Users": [
+    {
+      "id": 1,
+      "client_name": "SEKO",
+      "client_ID": "gg_106263452150533879793",
+      "client_DateExpired": "2028-12-22",
+      "Client_KeyAccecs": "XTPRIVATESKIN",
+      "Client_VisibleSkinPrivate": [4065, 4027, 4028, 4029]
+    },
+    {
+      "id": 2,
+      "client_name": "ARZU",
+      "client_ID": "gg_105466135235745460465",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4002, 4001]
+    },
+    {
+      "id": 3,
+      "client_name": "ACUSH",
+      "client_ID": "gg_113378472117769619724",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [515, 521, 1572, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]
+    },
+    {
+      "id": 4,
+      "client_name": "KRDNZ EROL",
+      "client_ID": "gg_109179414629321320753",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4004]
+    },
+    {
+      "id": 5,
+      "client_name": "ALMEDİO",
+      "client_ID": "gg_112483448448743298073",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4005, 4001, 4006, 4013]
+    },
+    {
+      "id": 6,
+      "client_name": "UNAL UCAR",
+      "client_ID": "gg_106776011071286938302",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4007, 4003, 4130]
+    },
+    {
+      "id": 7,
+      "client_name": "ZEKİİ",
+      "client_ID": "gg_114462066845902344686",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4131]
+    },
+    {
+      "id": 8,
+      "client_name": "ALİ GURSEL",
+      "client_ID": "gg_102764209641936225250",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4008]
+    },
+    {
+      "id": 9,
+      "client_name": "CARDONA",
+      "client_ID": "gg_114208392180316447264",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [502, 717, 508, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]
+    },
+    {
+      "id": 10,
+      "client_name": "TCHACER",
+      "client_ID": "gg_117397807898958770215",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4009]
+    },
+    {
+      "id": 11,
+      "client_name": "RAMİZ",
+      "client_ID": "gg_110344273426432487919",
+      "Client_KeyAccecs": "XTPRIVATESKIN",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4010]
+    },
+    {
+      "id": 12,
+      "client_name": "MUBARIIZ",
+      "client_ID": "gg_113303398669749677288",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4011]
+    },
+    {
+      "id": 13,
+      "client_name": "Castillo",
+      "client_ID": "gg_109590212314460506096",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [517, 1200, 1206, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]
+    },
+    {
+      "id": 14,
+      "client_name": "ENERCII",
+      "client_ID": "gg_102406237976287879304",
+      "client_DateExpired": "c",
+      "Client_VisibleSkinPrivate": [4073]
+    },
+    {
+      "id": 15,
+      "client_name": "LEVENT AGA",
+      "client_ID": "gg_113538446280819784811",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4072]
+    },
+    {
+      "id": 16,
+      "client_name": "HALO DAYI",
+      "client_ID": "gg_116667212371781389010",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4110]
+    },
+    {
+      "id": 17,
+      "client_name": "DF 1454",
+      "client_ID": "gg_110672583529704971982",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1454]
+    },
+    {
+      "id": 18,
+      "client_name": "DF TOMTOM",
+      "client_ID": "gg_110069795721383659155",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1456]
+    },
+    {
+      "id": 19,
+      "client_name": "ANKA ATABEY",
+      "client_ID": "gg_106263203218070035844",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4113]
+    },
+    {
+      "id": 20,
+      "client_name": "HUYSUZ",
+      "client_ID": "gg_115214568877783684555",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4014]
+    },
+    {
+      "id": 21,
+      "client_name": "GICIK BEY",
+      "client_ID": "gg_109224131538569505778",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4018]
+    },
+    {
+      "id": 22,
+      "client_name": "HSG SAMSUNG",
+      "client_ID": "gg_105681256585631345182",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [516, 2004, 2002]
+    },
+    {
+      "id": 23,
+      "client_name": "HSG JOHNEE",
+      "client_ID": "gg_106747500315816190359",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [518, 1462]
+    },
+    {
+      "id": 24,
+      "client_name": "KRDNZ EROL",
+      "client_ID": "gg_115613141167433343008",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4004, 4030, 4031, 4084]
+    },
+    {
+      "id": 25,
+      "client_name": "MEVO REİS",
+      "client_ID": "gg_109270133242864617203",
+      "client_DateExpired": "2028-12-22",
+      "Client_KeyAccecs": "Client_VisibleSkinPrivate",
+      "Client_VisibleSkinPrivate": [4027, 4028, 4029]
+    },
+    {
+      "id": 26,
+      "client_name": "SEKO SGLM",
+      "client_ID": "gg_106263452150533879793",
+      "client_DateExpired": "2025-02-22",
+      "Client_VisibleSkinPrivate": [4050, 4074, 4075]
+    },
+    {
+      "id": 27,
+      "client_name": "BORDO",
+      "client_ID": "gg_109524145042818220309",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4056, 4068, 4125]
+    },
+    {
+      "id": 28,
+      "client_name": "NAYZN REİS",
+      "client_ID": "gg_101381594472282494314",
+      "client_DateExpired": "2028-12-22",
+      "Client_VisibleSkinPrivate": [4052]
+    },
+    {
+      "id": 29,
+      "client_name": "CELAL REİS",
+      "client_ID": "gg_100683635075553789747",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4055]
+    },
+    {
+      "id": 30,
+      "client_name": "AKINCI",
+      "client_ID": "gg_105291818255067053263",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4057]
+    },
+    {
+      "id": 31,
+      "client_name": "KRDNZ ORHAN",
+      "client_ID": "gg_107061726893084251325",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4058]
+    },
+    {
+      "id": 32,
+      "client_name": "gezegen",
+      "client_ID": "gg_104139099081784717188",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4076]
+    },
+    {
+      "id": 33,
+      "client_name": "anka gonyalı",
+      "client_ID": "gg_109749113147899511677",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4069]
+    },
+    {
+      "id": 34,
+      "client_name": "buz52",
+      "client_ID": "gg_107625490055898866036",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4070, 4071]
+    },
+    {
+      "id": 35,
+      "client_name": "kosavalıı",
+      "client_ID": "gg_110251756575053175051",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4077]
+    },
+    {
+      "id": 36,
+      "client_name": "YILDO BABA",
+      "client_ID": "gg_110143086124933744274",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4078]
+    },
+    {
+      "id": 37,
+      "client_name": "rox ferit",
+      "client_ID": "gg_103585112222404805568",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4079]
+    },
+    {
+      "id": 38,
+      "client_name": "panda caan",
+      "client_ID": "gg_118003751567153802477",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4080]
+    },
+    {
+      "id": 39,
+      "client_name": "EMIR REİS",
+      "client_ID": "gg_116382653507326450203",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4085]
+    },
+    {
+      "id": 40,
+      "client_name": "TCEHAR",
+      "client_ID": "gg_102428886522908443984",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4009, 4064]
+    },
+    {
+      "id": 41,
+      "client_name": "RAMO REİS",
+      "client_ID": "gg_112120837151047324040",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4086]
+    },
+    {
+      "id": 42,
+      "client_name": "SİDAR REİS",
+      "client_ID": "gg_112347128815848719611",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4088]
+    },
+    {
+      "id": 43,
+      "client_name": "EREN REİS",
+      "client_ID": "gg_100118466522254215660",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4087]
+    },
+    {
+      "id": 44,
+      "client_name": "TUNAHAN REİS",
+      "client_ID": "gg_102340884801272504496",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4089]
+    },
+    {
+      "id": 45,
+      "client_name": "MUHTAR",
+      "client_ID": "gg_118011253178785134539",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4090, 4016, 4091, 4092]
+    },
+    {
+      "id": 46,
+      "client_name": "KANUNI ABİİ",
+      "client_ID": "gg_110180116710293109905",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4012]
+    },
+    {
+      "id": 47,
+      "client_name": "MRS",
+      "client_ID": "gg_112000101066586980089",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1858]
+    },
+    {
+      "id": 48,
+      "client_name": "MRS",
+      "client_ID": "gg_114035293331282705531",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1859]
+    },
+    {
+      "id": 49,
+      "client_name": "LMZ DYNOMITE",
+      "client_ID": "gg_112489902104387486414",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1955]
+    },
+    {
+      "id": 50,
+      "client_name": "MATADOR",
+      "client_ID": "gg_111138779018352104977",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4066]
+    },
+    {
+      "id": 51,
+      "client_name": "rooxgamıng",
+      "client_ID": "gg_107374682847034163077",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4126]
+    },
+    {
+      "id": 52,
+      "client_name": "AZER",
+      "client_ID": "gg_107711883797801008636",
+      "client_DateExpired": "2024-11-22",
+      "Client_VisibleSkinPrivate": [4127]
+    },
+    {
+      "id": 53,
+      "client_name": "SIMAV",
+      "client_ID": "gg_110536146782798414946",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4067]
+    },
+    {
+      "id": 54,
+      "client_name": "SİNAN 67",
+      "client_ID": "gg_110143467517730698768",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4128]
+    },
+    {
+      "id": 55,
+      "client_name": "SUSKUN",
+      "client_ID": "gg_116497656229571661305",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [4129]
+    },
+    {
+      "id": 56,
+      "client_name": "HOX KHALED",
+      "client_ID": "gg_109395786898582254614",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [2151]
+    },
+    {
+      "id": 57,
+      "client_name": "HOX",
+      "client_ID": "gg_112492524036887724990",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [2152]
+    },
+    {
+      "id": 58,
+      "client_name": "DF",
+      "client_ID": "gg_101387876291106559645",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1471]
+    },
+    {
+      "id": 59,
+      "client_name": "DF",
+      "client_ID": "gg_112200516650605441539",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1472]
+    },
+    {
+      "id": 60,
+      "client_name": "HSG EPTOS",
+      "client_ID": "gg_107364839111775974937",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]
+    },
+    {
+      "id": 61,
+      "client_name": "ALEMAN",
+      "client_ID": "gg_102696785992493985959",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]
+    },
+    {
+      "id": 62,
+      "client_name": "SUPERMOD",
+      "client_ID": "gg_106326634503367789425",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [2200]
+    },
+    {
+      "id": 63,
+      "client_name": "DF BATMAN",
+      "client_ID": "gg_113557709066701315116",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1473]
+    },
+    {
+      "id": 64,
+      "client_name": "YİGİT",
+      "client_ID": "gg_102601141305099468593",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [600]
+    },
+    {
+      "id": 65,
+      "client_name": "mahmud",
+      "client_ID": "gg_100112240467616542939",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [1309]
+    },
+    {
+      "id": 66,
+      "client_name": "HSG EPTOS",
+      "client_ID": "gg_107364839111775974937",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": [526]
+    },
+    {
+      "id": 67,
+      "client_name": "wahed",
+      "client_ID": "gg_114195640364145831147",
+      "client_DateExpired": "2025-12-22",
+      "Client_VisibleSkinPrivate": [601]
+    },
+    {
+      "id": 68,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 69,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 70,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 71,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 72,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 73,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 74,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 75,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 76,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 77,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 78,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 79,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 80,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    },
+    {
+      "id": 81,
+      "client_name": "",
+      "client_ID": "",
+      "client_DateExpired": "",
+      "Client_VisibleSkinPrivate": []
+    }
+  ]
+}
